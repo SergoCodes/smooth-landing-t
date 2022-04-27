69d1d41733fd4cdd82faf3140731a10b1b80d696
Sergo <sergo400ggg@gmail.com>
2022-04-28 00:40:10 +0300

    info sections

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,57 @@
+import img1 from '../../images/svg-1.svg'
+import img2 from '../../images/svg-2.svg'
+import img3 from '../../images/svg-3.svg'
+
+export const homeData = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Premium Bank',
+  headLine: 'Unlimited Transaction with zero fees',
+  description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged' +
+    'any fees.',
+  buttonLabel: 'Get started',
+  imgStart: false,
+  img: img1,
+  alt: 'Car',
+  dark: true,
+  primary: true,
+  darkText: false
+}
+
+export const aboutData = {
+  id: 'about',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Unlimited Access',
+  headLine: 'Unlimited Transaction with zero fees',
+  description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged' +
+    'any fees.',
+  buttonLabel: 'Learn More',
+  imgStart: true,
+  img: img2,
+  alt: 'House',
+  dark: false,
+  primary: false,
+  darkText: true
+}
+
+export const signupData = {
+  id: 'signup',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Premium Bank',
+  headLine: 'Unlimited Transaction with zero fees',
+  description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged' +
+    'any fees.',
+  buttonLabel: 'Get started',
+  imgStart: false,
+  img: img3,
+  alt: 'Car',
+  dark: true,
+  primary: true,
+  darkText: true
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 import HeroSection from '../components/HeroSection'
+import InfoSection from '../components/InfoSection'
+import {aboutData, homeData, signupData} from '../components/InfoSection/Data'
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -14,6 +16,9 @@ const Home = () => {
       <Navbar toggle={toggle}/>
       <Sidebar isOpen={isOpen} toggle={toggle}/>
       <HeroSection/>
+      <InfoSection {...homeData}/>
+      <InfoSection {...aboutData}/>
+      <InfoSection {...signupData}/>
     </>
   );
 };
