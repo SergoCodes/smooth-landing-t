e751094627e4e37369b6fc23f45edb354770145b
Sergo <sergo400ggg@gmail.com>
2022-04-28 02:12:38 +0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css'
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Home from './pages'
+import SigninPage from './pages/signin'
 
 const App = () => {
   return (
     <Router>
-      <Home/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/signin' element={<SigninPage/>}/>
+      </Routes>
+      
     </Router>
   );
 };
